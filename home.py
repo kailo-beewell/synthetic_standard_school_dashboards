6e5f4e9c47e7791a216fa12627cb844f53a2ea26
amyheather <a.heather2@exeter.ac.uk>
2023-11-13 14:01:18 +0000

    Moved switch page button to utilities

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
+from utilities.switch_page_button import switch_page
 
 st.set_page_config(
     page_title='#BeeWell School Dashboard',
@@ -14,39 +15,6 @@ illustration = Image.open('images/levelling-the-ground.jpg')
 # Need to change to globally set school depending on login
 school = st.radio('School', ['School A', 'School B', 'School C', 'School D',
                              'School E', 'School F', 'School G'])
-
-# Need to move this into a functions section
-def switch_page(page_name: str):
-    '''
-    Switch page programmatically in a multipage app
-    Copied from streamlit_extras: https://github.com/arnaudmiribel/streamlit-extras
-
-    Args:
-        page_name (str): Target page name
-    '''
-    from streamlit.runtime.scriptrunner import RerunData, RerunException
-    from streamlit.source_util import get_pages
-
-    def standardize_name(name: str) -> str:
-        return name.lower().replace("_", " ")
-
-    page_name = standardize_name(page_name)
-
-    pages = get_pages('home.py')
-
-    for page_hash, config in pages.items():
-        if standardize_name(config["page_name"]) == page_name:
-            raise RerunException(
-                RerunData(
-                    page_script_hash=page_hash,
-                    page_name=page_name,
-                )
-            )
-
-    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
-
-    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
-
 
 st.title(school)
 
