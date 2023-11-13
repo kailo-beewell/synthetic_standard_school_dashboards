590b2b1a0837b729881cd5cefbc518fdaa891267
amyheather <a.heather2@exeter.ac.uk>
2023-11-13 14:37:48 +0000

    Setup page config with helper function and add school size from data

--- home.py
+++ home.py
@@ -2,24 +2,32 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from utilities.switch_page_button import switch_page
+from utilities.fixed_params import page_setup
 
-st.set_page_config(
-    page_title='#BeeWell School Dashboard',
-    page_icon='🐝',
-    initial_sidebar_state='expanded'
-)
+# Set page configuration
+page_setup()
 
+# Import data and images used on this page
 data = pd.read_csv('data/survey_data/aggregate_scores.csv')
 illustration = Image.open('images/levelling-the-ground.jpg')
 
 # Need to change to globally set school depending on login
-school = st.radio('School', ['School A', 'School B', 'School C', 'School D',
+school_name = st.radio('School', ['School A', 'School B', 'School C', 'School D',
                              'School E', 'School F', 'School G'])
 
-st.title(school)
+# Find school size
+school_size = data.loc[
+    (data['school_lab'] == school_name) &
+    (data['variable'] == 'overall_count') &
+    (data['year_group_lab'] == 'All') &
+    (data['gender_lab'] == 'All') &
+    (data['fsm_lab'] == 'All') &
+    (data['sen_lab'] == 'All'), 'count'].values[0].astype(int)
+
+st.title(school_name)
 
 # Need to move this into a text section
-st.markdown('Thank for taking part in the #BeeWell survey. At your school, n pupils completed the survey.')
+st.markdown(f'Thank for taking part in the #BeeWell survey. At your school, {school_size} pupils completed the survey.')
 st.markdown('''
 Your survey results are provided, including comparison against:
 * Other schools in Northern Devon
