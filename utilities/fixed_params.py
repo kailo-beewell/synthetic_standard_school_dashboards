379f4585b92c081232dce397f04c3d2110dec82b
amyheather <a.heather2@exeter.ac.uk>
2023-11-15 15:50:23 +0000

    Modified page width and added consistent about description for when click three dots in top right corner

--- utilities/fixed_params.py
+++ utilities/fixed_params.py
@@ -8,4 +8,5 @@ def page_setup():
         page_title='#BeeWell School Dashboard',
         page_icon='🐝',
         initial_sidebar_state='expanded',
-        layout='wide')
+        layout='centered',
+        menu_items={'About': 'Dashboard for schools completing the standard version of the #BeeWell survey in North Devon and Torridge in 2023/24.'})
