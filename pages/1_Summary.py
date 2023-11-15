65c27c7b997e2c777da6492d837a8642717018b0
amyheather <a.heather2@exeter.ac.uk>
2023-11-15 13:58:22 +0000

    Applied st warning issue boxes for all RAG, updated pivot for relabelled data, renamed headings

--- pages/1_Summary.py
+++ pages/1_Summary.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import streamlit as st
 from utilities.fixed_params import page_setup
@@ -12,7 +13,7 @@ school = st.selectbox(
 
 st.title('''Your school's results''')
 
-# Chosen data
+##########################################################
 
 # Import data
 data = pd.read_csv('data/survey_data/aggregate_scores_rag.csv')
@@ -28,18 +29,18 @@ fsm = ['All']
 sen = ['All']
 if chosen_group == 'By year group':
     pivot_var = 'year_group_lab'
-    year_group = ['8.0', '10.0']
+    year_group = ['Year 8', 'Year 10']
 elif chosen_group == 'By gender':
     pivot_var = 'gender_lab'
-    gender = ['Boy', 'Currently unsure', 'Girl',
-              'I describe myself in another way', 'Non-binary',
-              'Prefer not to say']
+    gender = ['Boy', 'Girl']
+              #'I describe myself in another way', 'Non-binary',
+              #'Prefer not to say']
 elif chosen_group == 'By FSM':
     pivot_var = 'fsm_lab'
-    fsm = ['Yes', 'No']
+    fsm = ['FSM', 'Non-FSM']
 elif chosen_group == 'By SEN':
     pivot_var = 'sen_lab'
-    sen = ['Yes', 'No']
+    sen = ['SEN', 'Non-SEN']
 
 # Filter data
 chosen = data[
@@ -48,76 +49,38 @@ chosen = data[
     (data['gender_lab'].isin(gender)) &
     (data['fsm_lab'].isin(fsm)) &
     (data['sen_lab'].isin(sen)) &
-    (~data['variable'].isin(['birth_you_age_score', 'overall_count']))]
+    (~data['variable'].isin([
+        'birth_you_age_score', 'overall_count', 'staff_talk_score',
+        'home_talk_score', 'peer_talk_score']))]
 
 # ISSUE: This reorders the columns
 if chosen_group != 'All pupils':
-    chosen = chosen[['variable', pivot_var, 'rag']].pivot(
-        index='variable', columns=pivot_var, values='rag').reset_index()
+    chosen = chosen[['variable_lab', pivot_var, 'rag']].pivot(
+        index='variable_lab', columns=pivot_var, values='rag').reset_index()
 else:
-    chosen = chosen[['variable', 'rag']]
+    chosen = chosen[['variable_lab', 'rag']]
 
 ##########################################################
 
 # Show the detail pages on the sidebar when on summary or a detail page
 page = st.sidebar.radio(
     '''Your school's results''',
-    options=['All results'] + chosen['variable'].to_list())
+    options=['All results'] + chosen['variable_lab'].to_list())
 
 ##########################################################
-
-##########################################################
-
-# METHOD 1: Dataframes
-
-# Show dataframe
-st.dataframe(chosen, use_container_width=True, hide_index=True)
-
-##########################################################
-
-col_widths = [0.3, 0.3, 0.4]
-
-col1, col2, col3 = st.columns(col_widths)
-with col1:
-    st.markdown('**Topic**')
-with col2:
-    st.markdown('**All pupils** (box)')
-with col3:
-    st.markdown('**All pupils** (emoji)')
-
-col1, col2, col3 = st.columns(col_widths)
-with col1:
-    st.markdown('')
-    st.markdown('**Autonomy**', help='''Autonomy is about how 'in control' students feel about their lives''')
-with col2:
-    st.success('Above average', icon='✅')
-with col3:
-    st.markdown('✅')
-
-col1, col2, col3 = st.columns(col_widths)
-with col1:
-    st.markdown('')
-    st.markdown('**Life satisfaction**', help='''Life satisfaction is about how satisfied students feel with their lives''')
-with col2:
-    st.warning('Average')
-with col3:
-    st.warning('🟡')
-
-col1, col2, col3 = st.columns(col_widths)
-with col1:
-    st.markdown('')
-    st.markdown('**Optimism**', help='''Optimism is about how hopeful and confident students feel about the future''')
-with col2:
-    st.error('↓ Below average')
-with col3:
-    st.markdown('❗')
-
-##########################################################
-
-# METHOD 2: Columns iterative
 
 # Set number of columns
 ncol = len(chosen.columns)
+
+# Rename columns if in the dataframe
+colnames = {
+    'variable_lab': 'Topic',
+    'rag': 'All pupils',
+}
+chosen = chosen.rename(columns=colnames)
+
+# TO ADD: Set the dimensions of columns (e.g. [0.4, 0.4, 0.2] for each ncol number)
+# Would then be st.columns(col_widths) e.g. st.columns([0.4, 0.4, 0.2])
 
 # Add column names
 cols = st.columns(ncol)
@@ -130,5 +93,13 @@ for index, row in chosen.iterrows():
     cols = st.columns(ncol)
     for i in range(ncol):
         with cols[i]:
-            st.markdown(row[i])
+            if row[i] == 'below':
+                st.error('↓ Below average')
+            elif row[i] == 'average':
+                st.warning('~ Average')
+            elif row[i] == 'above':
+                st.success('↑ Above average')
+            else:
+                st.markdown('')
+                st.markdown(row[i])
 
