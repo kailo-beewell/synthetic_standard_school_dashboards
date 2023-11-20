6fa7b94dd49aa76dbaa76d69c67cbc8119d33c67
amyheather <a.heather2@exeter.ac.uk>
2023-11-20 14:24:58 +0000

    Change page width between pages

--- utilities/fixed_params.py
+++ utilities/fixed_params.py
@@ -3,10 +3,15 @@ Helper function for setting up the page, with the same settings for every page.
 '''
 import streamlit as st
 
-def page_setup():
+def page_setup(layout):
+    '''
+    Set up page to standard conditions, with layout as specified
+    Inputs:
+    - layout: string, 'wide' or 'centered'
+    '''
     st.set_page_config(
         page_title='#BeeWell School Dashboard',
         page_icon='🐝',
         initial_sidebar_state='expanded',
-        layout='centered',
+        layout=layout,
         menu_items={'About': 'Dashboard for schools completing the standard version of the #BeeWell survey in North Devon and Torridge in 2023/24.'})
