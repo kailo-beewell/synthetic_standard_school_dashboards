d6ec56a0b5f7c7e611d3710e10c7f1c95a968504
amyheather <a.heather2@exeter.ac.uk>
2023-11-23 15:42:27 +0000

    Moved stacked bar chart to utilities as a function

--- pages/Details.py
+++ pages/Details.py
@@ -3,9 +3,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import textwrap as tr
 from utilities.fixed_params import page_setup
-from utilities.colours import linear_gradient
+from utilities.details import details_stacked_bar
 
 # Set page configuration
 page_setup('wide')
@@ -59,115 +58,8 @@ for index, row in chosen.iterrows():
     df_list.append(df)
 chosen_result = pd.concat(df_list)
 
-# Get colour spectrum between the provided colours, for all except one category
-# Use 'cat_lab' rather than 'cat' as sometimes cat is 0-indexed or 1-indexed
-start_colour = '#2A52BE'
-end_colour = '#D4DCF2'
-n_cat = chosen_result['cat_lab'].drop_duplicates().size
-colours = linear_gradient(start_colour, end_colour, n_cat-1)['hex']
-# Add final colour of grey for the last category, which will be "missing"
-colours += ['#DDDDDD']
-
-# Convert labels to wrapped lines, with max length and new lines \n
-def wrap_text(string, width):
-    '''
-    Wrap the provided string to the specified width, producing a single string
-    with new lines indicated by '<br>'. If string length is less than the
-    specified width, add blank space to the start of the string (so it will
-    still occupy the same amount of space on the chart)
-    '''
-    # Wrap string with new lines indicated by <br>
-    wrap = '<br>'.join(tr.wrap(string, width=width))
-    # If the whole string is less than the chosen width, at blank spaces to
-    # the start to it reaches that width
-    if len(wrap) < width:
-        blank = width - len(wrap)
-        wrap=(' '*blank) + wrap
-    # If string is greater than width (will therefore have breaks), at blank
-    # space to the beginning of the first line so that it reaches the 
-    # maximum width
-    elif len(wrap) > width:
-        first_line = wrap.split('<br>')[0]
-        if len(first_line) < width:
-            blank = width - len(first_line)
-            wrap=(' '*blank) + wrap
-    return(wrap)
-
-
-chosen_result['measure_lab_wrap'] = chosen_result['measure_lab'].apply(
-    lambda x: wrap_text(x, 50))
-
-# Create plot
-fig = px.bar(
-    chosen_result, x='percentage', y='measure_lab_wrap', color='cat_lab',
-    text_auto=True, hover_data=['count', 'measure_lab'], orientation='h',
-    color_discrete_sequence=colours)
-
-# Add percent sign to the numbers labelling the bars
-fig.for_each_trace(lambda t: t.update(texttemplate = t.texttemplate + ' %'))
-
-# Set x axis ticks to include % sign, and remove the axis titles
-fig.update_layout(xaxis = dict(
-    tickmode='array',
-    tickvals=[0, 20, 40, 60, 80, 100],
-    ticktext=['0%', '20%', '40%', '60%', '80%', '100%'],
-    title=''))
-fig.update_layout(yaxis_title=None)
-
-# Set font size
-font_size = 18
-fig.update_layout(
-    font = dict(size=font_size),
-    xaxis = dict(tickfont=dict(size=font_size)),
-    yaxis = dict(tickfont=dict(size=font_size)),
-    legend = dict(font_size=font_size)
-)
-
-# Find number of variables being plot, then set height of figure based on that
-# so the bars appear to be fairly consistent height between different charts
-n_var = chosen_result['measure_lab'].drop_duplicates().size
-height = {
-    1: 200,
-    2: 270,
-    3: 350,
-    4: 400,
-    5: 500,
-    6: 600,
-    7: 600,
-    8: 700,
-    9: 800,
-    10: 900
-}
-fig.update_layout(autosize=True, height=height[n_var])
-
-# Make legend horizontal and center on 0.5, and lower on y so not overlapping
-# with the title (which have to adjust depending on nvar, with values
-# identified manually through trial and error)
-y_pos = {
-    1: -2,
-    2: -0.3,
-    3: -0.2,
-    4: -0.17,
-    5: -0.15,
-    6: -0.13,
-    7: -0.1,
-    8: -0.1,
-    9: -0.1,
-    10: -0.1
-}
-fig.update_layout(legend=dict(
-    orientation='h',
-    x=0.5,
-    xanchor='center',
-    y=y_pos[n_var],
-    title=''))
-
-# Disable zooming and panning
-fig.layout.xaxis.fixedrange = True
-fig.layout.yaxis.fixedrange = True
-
-# Show plot
-st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
+# Create stacked bar chart
+details_stacked_bar(chosen_result)
 
 ###############################################################################
 # Initial basic example of doing the comparator chart between schools...
