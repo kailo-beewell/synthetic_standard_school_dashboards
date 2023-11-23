bc9c5e0b9efa19f6e087c3465ea11a7481e2a1ba
amyheather <a.heather2@exeter.ac.uk>
2023-11-23 15:33:32 +0000

    Stacked bar (1) blank text to start of wrapped y axis labels to help improve sizing consistency, (2) x axis ticks have percent sign, (3) font size larger, (4) return to setting bar size using overall figure size but with manual sizes for height and legend positioning as otherwise its messy

--- pages/Details.py
+++ pages/Details.py
@@ -76,10 +76,21 @@ def wrap_text(string, width):
     specified width, add blank space to the start of the string (so it will
     still occupy the same amount of space on the chart)
     '''
+    # Wrap string with new lines indicated by <br>
     wrap = '<br>'.join(tr.wrap(string, width=width))
+    # If the whole string is less than the chosen width, at blank spaces to
+    # the start to it reaches that width
     if len(wrap) < width:
         blank = width - len(wrap)
         wrap=(' '*blank) + wrap
+    # If string is greater than width (will therefore have breaks), at blank
+    # space to the beginning of the first line so that it reaches the 
+    # maximum width
+    elif len(wrap) > width:
+        first_line = wrap.split('<br>')[0]
+        if len(first_line) < width:
+            blank = width - len(first_line)
+            wrap=(' '*blank) + wrap
     return(wrap)
 
 
@@ -95,16 +106,61 @@ fig = px.bar(
 # Add percent sign to the numbers labelling the bars
 fig.for_each_trace(lambda t: t.update(texttemplate = t.texttemplate + ' %'))
 
-# Remove x and y axes as they are redundant
-fig.update_layout(yaxis_title=None, xaxis_title='Percentage')
+# Set x axis ticks to include % sign, and remove the axis titles
+fig.update_layout(xaxis = dict(
+    tickmode='array',
+    tickvals=[0, 20, 40, 60, 80, 100],
+    ticktext=['0%', '20%', '40%', '60%', '80%', '100%'],
+    title=''))
+fig.update_layout(yaxis_title=None)
 
-# Find number of variables being plot, and set bar size based on that,
-# so it is fairly consistent between different charts
+# Set font size
+font_size = 18
+fig.update_layout(
+    font = dict(size=font_size),
+    xaxis = dict(tickfont=dict(size=font_size)),
+    yaxis = dict(tickfont=dict(size=font_size)),
+    legend = dict(font_size=font_size)
+)
+
+# Find number of variables being plot, then set height of figure based on that
+# so the bars appear to be fairly consistent height between different charts
 n_var = chosen_result['measure_lab'].drop_duplicates().size
-bar_width = n_var*0.1
-if bar_width == 1:
-    bar_width = 0.9
-fig.update_traces(width=bar_width)
+height = {
+    1: 200,
+    2: 270,
+    3: 350,
+    4: 400,
+    5: 500,
+    6: 600,
+    7: 600,
+    8: 700,
+    9: 800,
+    10: 900
+}
+fig.update_layout(autosize=True, height=height[n_var])
+
+# Make legend horizontal and center on 0.5, and lower on y so not overlapping
+# with the title (which have to adjust depending on nvar, with values
+# identified manually through trial and error)
+y_pos = {
+    1: -2,
+    2: -0.3,
+    3: -0.2,
+    4: -0.17,
+    5: -0.15,
+    6: -0.13,
+    7: -0.1,
+    8: -0.1,
+    9: -0.1,
+    10: -0.1
+}
+fig.update_layout(legend=dict(
+    orientation='h',
+    x=0.5,
+    xanchor='center',
+    y=y_pos[n_var],
+    title=''))
 
 # Disable zooming and panning
 fig.layout.xaxis.fixedrange = True
