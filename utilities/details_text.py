00aa97501b932cd1a75a820066ac9c25b28957de
amyheather <a.heather2@exeter.ac.uk>
2023-12-05 13:28:35 +0000

    Added more descriptions

--- utilities/details_text.py
+++ utilities/details_text.py
@@ -25,15 +25,86 @@ def create_stacked_descrip():
 
         'wellbeing': '''
 **Question:** Pupils were asked to choose the response that best describes their experience of the following statements over the last 2 weeks.  \n
-**Interpretation:** From left to right, responses range from:  
+**Interpretation:** From left to right, responses range from:
 * 'None of the time' = **lower** wellbeing levels, to  
 * 'All of the time' = **higher** wellbeing levels''',
 
+        'esteem': '''
+**Question:** Pupils were asked how strongly they agreed or disagreed with the following statements about themselves now.  \n
+**Interpretation:** From left to right, responses range from:
+* 'Strongly disagree' = **lower** levels of self-esteem
+* 'Strongly agree' = **higher** levels of self-esteem''',
+
         'stress': '''
-**Question:** Pupils were asked how often the felt the following statements in the last month.  \n
-**Interpretation:** From left to right, responses range from:  
-* 'Never' - Feeling **more stressed** and struggling to cope, to  
-* 'Very Often' - Feeling **less stressed** and better able to cope''',
+**Question:** Pupils were asked how often they felt/thought the following statements during the last month.  \n
+**Interpretation:** From left to right, responses range from:
+* 'Never' = Feeling **more** stressed and struggling to cope, to  
+* 'Very Often' = Feeling **less** stressed and better able to cope''',
+
+        'appearance_happy': '''
+**Interpretation:** From left to right, responses range from:
+* '0 - Very unhappy'
+* '10 - Very happy'  \n
+Pupils were also able to respond 'prefer not to say'.''',
+
+        'appearance_feel': '''
+**Interpretation:** From left to right, responses range from:
+* 'Strongly agree' = appearance **absolutely** affects how feel about self
+* 'Strongly disagree' = appearance **does not** affect how feel about self''',
+
+        'negative': '''
+**Question:** Pupils were asked which answer was best for them for the following statements about how they feel.  \n
+**Interpretation:** From left to right, responses range from:
+* 'Always' = **more** experience of emotional difficulties
+* 'Never' = **less** experience of emotional difficulties''',
+
+        'lonely': '''
+**Interpretation:** From left to right, responses range from:
+* 'Often or always' = **more** lonely
+* 'Never' = **less** lonely''',
+
+        'support': '''
+**Question:** Pupils were asked how strongly they agree or disagree with the following statements.  \n
+**Interpretation:** From left to right, responses range from:
+* 'Strongly disagree' = **less** knowledge on supporting themselves and finding advice
+* 'Strongly agree' = **more** knowledge on supporting themselves and finding advice''',
+
+        'sleep': '''''',
+
+        'physical_days': '''
+**Question:** Young people were told that this question was about their physical \
+activity. It stated that we were 'particularly interested in activity that \
+increases your heart rate and makes you get out of breath some of the time. \
+Physical activity can be done in sports, school activities, playing with \
+friends, or walking and cycling to school or other places.'  
+**Interpretation:** From left to right, responses range from **less** to **more** days of physical activity per week.
+''',
+
+        'physical_hours': '''
+**Question:** Young people were asked to think about the days when they are \
+physically active, and to think about all the different activities they \
+typically do over the course of the day - and were then asked the following question.  
+**Interpretation:** From left to right, responses range from **shorter** to **longer** amounts of time spend doing physical activity.
+''',
+
+        'free_like': '''
+**Interpretation:** From left to right, responses range from:
+* 'Almost never' = **less** able to do things they like in their free time
+* 'Almost always' = **more** able to do things they like in their free time
+''',
+
+        'media': '''
+**Question:** Young people were asked how many hours they would normally spend \
+on social media per day, with examples of sites or apps like TikTok, Instagram, and Snapchat.  
+**Interpretation:** Responses range from **7+ hours** per day to **0 hours** per day.''',
+
+        'places_freq': '''''',
+
+        'places_barriers': '''
+**Question:** Pupils were asked if there are any reasons that prevent them \
+from doing activities or going to places that they want to in their free time. \
+They were asked to select all that apply.
+''',
 
         'home_happy': '''
 This question is about how happy young people are with the home they live in.  
