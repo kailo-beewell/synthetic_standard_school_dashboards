3a67cb28be382ec4bbf7871872f0fc71aafed7f4
amyheather <a.heather2@exeter.ac.uk>
2023-12-13 16:30:26 +0000

    Moved logo to bottom of page in footer

--- utilities/fixed_params.py
+++ utilities/fixed_params.py
@@ -8,12 +8,33 @@ def page_setup():
     '''
     Set up page to standard conditions, with layout as specified
     '''
+    # Set up streamlit page parameters
     st.set_page_config(
         page_title='#BeeWell School Dashboard',
         page_icon='🐝',
         initial_sidebar_state='expanded',
         layout='centered',
         menu_items={'About': 'Dashboard for schools completing the standard version of the #BeeWell survey in North Devon and Torridge in 2023/24.'})
+
+    # Temporary: manual force School B when open page
+    st.session_state.school = 'School B'
+
+    # Import CSS style
+    with open('css/style.css') as css:
+        st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+
+def page_footer():
+    '''
+    Create footer for each page
+    '''
+    # Blank space
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
 
     # Set up logo for display in markdown, which we use instead of st.image()
     # to allow inline display and alt_text
@@ -22,24 +43,12 @@ def page_setup():
     url = base64.b64encode(contents).decode('utf-8')
     file.close()
 
-    # Temporary: manual force School B when open page
-    st.session_state.school = 'School B'
+    # Display logo
+    st.markdown(f'''
+<div style='text-align: center;'>
 
-    # Add logo and school name to top of each page
-    with st.sidebar:
-        st.markdown(f'''<img src='data:image/png;base64,{url}' 
-                    alt='#BeeWell delivered by Kailo in Northern Devon'>''',
-                    unsafe_allow_html=True)
-        # Manually set school (will need to change to set globally on login)
-        st.session_state.school = st.radio(
-            label='School (for testing)',
-            options=['School A', 'School B', 'School C', 'School D',
-                     'School E', 'School F', 'School G'],
-            index=1)
+<img src='data:image/png;base64,{url}' 
+alt='#BeeWell delivered by Kailo in Northern Devon'>
 
-    # Blank space
-    st.markdown('')
-
-    # Import CSS style
-    with open('css/style.css') as css:
-        st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+</div>
+''', unsafe_allow_html=True)
