9e1b3220a6325657edce4230ba83d80f5807c604
amyheather <a.heather2@exeter.ac.uk>
2023-12-13 16:05:54 +0000

    Modify sidebar and setup

--- utilities/fixed_params.py
+++ utilities/fixed_params.py
@@ -15,15 +15,30 @@ def page_setup():
         layout='centered',
         menu_items={'About': 'Dashboard for schools completing the standard version of the #BeeWell survey in North Devon and Torridge in 2023/24.'})
 
-    # Add logo to sidebar, using markdown rather than st.sidebar.image as that
-    # allows you to add alt_text for the image
+    # Set up logo for display in markdown, which we use instead of st.image()
+    # to allow inline display and alt_text
     file = open('./images/kailo_beewell_logo.png', 'rb')
     contents = file.read()
     url = base64.b64encode(contents).decode('utf-8')
     file.close()
-    st.sidebar.markdown(f'''
-<img src='data:image/png;base64,{url}' 
-alt='#BeeWell delivered by Kailo in Northern Devon'>''', unsafe_allow_html=True)
+
+    # Temporary: manual force School B when open page
+    st.session_state.school = 'School B'
+
+    # Add logo and school name to top of each page
+    with st.sidebar:
+        st.markdown(f'''<img src='data:image/png;base64,{url}' 
+                    alt='#BeeWell delivered by Kailo in Northern Devon'>''',
+                    unsafe_allow_html=True)
+        # Manually set school (will need to change to set globally on login)
+        st.session_state.school = st.radio(
+            label='School (for testing)',
+            options=['School A', 'School B', 'School C', 'School D',
+                     'School E', 'School F', 'School G'],
+            index=1)
+
+    # Blank space
+    st.markdown('')
 
     # Import CSS style
     with open('css/style.css') as css:
