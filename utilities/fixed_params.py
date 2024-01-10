34a3abc0ab2ea135df127df535b7dd13c99ec841
amyheather <a.heather2@exeter.ac.uk>
2024-01-10 15:51:03 +0000

    Page logo in sidebar rather than footer

--- utilities/fixed_params.py
+++ utilities/fixed_params.py
@@ -4,6 +4,31 @@ Helper function for setting up the page, with the same settings for every page.
 import base64
 import streamlit as st
 
+def page_logo():
+    '''
+    Create logo to go above the pages in the sidebar
+    '''
+    # Set up logo for display in markdown, which we use instead of st.image()
+    # to allow inline display and alt_text
+    file = open('./images/kailo_beewell_logo_padded.png', 'rb')
+    contents = file.read()
+    url = base64.b64encode(contents).decode('utf-8')
+    file.close()
+
+    # Display logo
+    st.markdown(f'''
+<style>
+    [data-testid='stSidebarNav'] {{
+        background-image: url('data:image/png;base64,{url}');
+        background-repeat: no-repeat;
+        padding-top: 110px; /* Move page names down */
+        background-position: 0px 50px; /* Move image down */
+        background-size: 240px;
+    }}
+    
+</style>
+''', unsafe_allow_html=True)
+
 def page_setup():
     '''
     Set up page to standard conditions, with layout as specified
@@ -23,32 +48,5 @@ def page_setup():
     with open('css/style.css') as css:
         st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
-
-def page_footer():
-    '''
-    Create footer for each page
-    '''
-    # Blank space
-    st.text('')
-    st.text('')
-    st.text('')
-    st.text('')
-    st.text('')
-    st.text('')
-
-    # Set up logo for display in markdown, which we use instead of st.image()
-    # to allow inline display and alt_text
-    file = open('./images/kailo_beewell_logo.png', 'rb')
-    contents = file.read()
-    url = base64.b64encode(contents).decode('utf-8')
-    file.close()
-
-    # Display logo
-    st.markdown(f'''
-<div style='text-align: center;'>
-
-<img src='data:image/png;base64,{url}' 
-alt='#BeeWell delivered by Kailo in Northern Devon'>
-
-</div>
-''', unsafe_allow_html=True)
+    # Add page logo
+    page_logo()
