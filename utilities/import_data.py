2600b2e39e43df1358f57aaf46eb03291c774555
amyheather <a.heather2@exeter.ac.uk>
2024-01-31 15:15:24 +0000

    Connect using pymysql rather than mysqlclient as that didn't work with Community Cloud. Pymysql requires CA cert, and as you can't pass the .pem file, have set up so it generates a temporary secure PEM file using contents provided in the secrets file.

--- utilities/import_data.py
+++ utilities/import_data.py
@@ -6,6 +6,24 @@ import pandas as pd
 import streamlit as st
 from pandas.testing import assert_frame_equal
 
+from tempfile import NamedTemporaryFile
+#import os
+import pymysql
+
+def get_connection():
+    db_conf = {
+        "host": st.secrets.tidb.host,
+        "user": st.secrets.tidb.username,
+        "password": st.secrets.tidb.password,
+        "database": st.secrets.tidb.database,
+        "port": st.secrets.tidb.port,
+        "ssl_verify_cert": True,
+        "ssl_verify_identity": True,
+        "ssl_ca": st.secrets.tidb.ssl_ca
+    }
+    return pymysql.connect(**db_conf)
+
+
 def import_tidb_data(tests=False):
     '''
     Imports all the datasets from TiDB Cloud, fixes any data type issues, and
@@ -17,41 +35,62 @@ def import_tidb_data(tests=False):
         Whether to run tests to check the data imported from TiDB cloud matches
         the CSV files in the GitHub repository
     '''
-    # Set up connection
-    conn = st.connection('tidb', type='sql')
+    # Create temporary PEM file for setting up the connection
+    with NamedTemporaryFile(suffix='.pem') as temp:
 
-    # Scores
-    if 'scores' not in st.session_state:
-        scores = conn.query('SELECT * FROM aggregate_scores;')
-        st.session_state['scores'] = scores
+        # Write the temporary file
+        temp.write(st.secrets.tidb.root_cert.encode('utf-8'))
 
-    # Scores RAG
-    if 'scores_rag' not in st.session_state:
-        scores_rag = conn.query('SELECT * FROM aggregate_scores_rag;')
-        # Convert columns to numric
-        to_fix = ['mean', 'count', 'total_pupils', 'group_n', 
-                'group_wt_mean', 'group_wt_std', 'lower', 'upper']
-        for col in to_fix:
-            scores_rag[col] = pd.to_numeric(scores_rag[col], errors='ignore')
-        # Convert string 'nan' to actual np.nan
-        scores_rag['rag'] = scores_rag['rag'].replace('nan', np.nan)
-        st.session_state['scores_rag'] = scores_rag
+        # Temporary file have pointer to current position in file - as we have
+        # just written, the pointer is at the end of the last write, so if you
+        # don't seek, you would read from the end of the file and find nothing
+        temp.seek(0)
 
-    # Responses
-    if 'responses' not in st.session_state:
-        responses = conn.query('SELECT * FROM aggregate_responses;')
-        st.session_state['responses'] = responses
+        # Set up connection manually, providing the temporary PEM file
+        # (as cannot use st.connection() without providing tempfile name in secrets)
+        conn = pymysql.connect(
+            host = st.secrets.tidb.host,
+            user = st.secrets.tidb.username,
+            password = st.secrets.tidb.password,
+            database = st.secrets.tidb.database,
+            port = st.secrets.tidb.port,
+            ssl_verify_cert = False,
+            ssl_verify_identity = False,
+            ssl_ca = temp.name
+        )
 
-    # Overall counts
-    if 'counts' not in st.session_state:
-        counts = conn.query('SELECT * FROM overall_counts;')
-        counts['count'] = pd.to_numeric(counts['count'], errors='ignore')
-        st.session_state['counts'] = counts
+        # Scores
+        if 'scores' not in st.session_state:
+            scores = pd.read_sql('SELECT * FROM aggregate_scores;', conn)
+            st.session_state['scores'] = scores
 
-    # Demographic
-    if 'demographic' not in st.session_state:
-        demographic = conn.query('SElECT * FROM aggregate_demographic;')
-        st.session_state['demographic'] = demographic
+        # Scores RAG
+        if 'scores_rag' not in st.session_state:
+            scores_rag = pd.read_sql('SELECT * FROM aggregate_scores_rag;', conn)
+            # Convert columns to numeric
+            to_fix = ['mean', 'count', 'total_pupils', 'group_n', 
+                    'group_wt_mean', 'group_wt_std', 'lower', 'upper']
+            for col in to_fix:
+                scores_rag[col] = pd.to_numeric(scores_rag[col], errors='ignore')
+            # Convert string 'nan' to actual np.nan
+            scores_rag['rag'] = scores_rag['rag'].replace('nan', np.nan)
+            st.session_state['scores_rag'] = scores_rag
+
+        # Responses
+        if 'responses' not in st.session_state:
+            responses = pd.read_sql('SELECT * FROM aggregate_responses;', conn)
+            st.session_state['responses'] = responses
+
+        # Overall counts
+        if 'counts' not in st.session_state:
+            counts = pd.read_sql('SELECT * FROM overall_counts;', conn)
+            counts['count'] = pd.to_numeric(counts['count'], errors='ignore')
+            st.session_state['counts'] = counts
+
+        # Demographic
+        if 'demographic' not in st.session_state:
+            demographic = pd.read_sql('SElECT * FROM aggregate_demographic;', conn)
+            st.session_state['demographic'] = demographic
 
     # Run tests to check whether these match the csv files
     if tests:
